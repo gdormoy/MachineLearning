de6128e01dd1b88a2f223b5c51fea99311251d8e
Thomas Gianfalla <gianfalla.t@gmail.com>
2020-02-17 18:13:36 +0100

    add project Unity + update gitignore

---
+++ Unity_Project/Assets/RetrieveAndModifySpherePositionsScript.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
+{
+    public Transform[] trainingSpheres;
+
+    public Transform[] testSpheres;
+
+
+    private double[] trainingInputs;
+
+    private double[] trainingExpectedOutputs;
+
+    private IntPtr model;
+
+    public void ReInitialize()
+    {
+        for (var i = 0; i < testSpheres.Length; i++)
+        {
+            testSpheres[i].position = new Vector3(
+                testSpheres[i].position.x,
+                0f,
+                testSpheres[i].position.z);
+        }
+    }
+    
+    public void CreateModel()
+    {
+        //model = CreateLinearModel(2, 1);
+    }
+
+    public void Train()
+    {
+        trainingInputs = new double[trainingSpheres.Length * 2];
+        trainingExpectedOutputs = new double[trainingSpheres.Length];
+
+        for (var i = 0; i < trainingSpheres.Length; i++)
+        {
+            trainingInputs[2 * i] = trainingSpheres[i].position.x;
+            trainingInputs[2 * i + 1] = trainingSpheres[i].position.z;
+            trainingExpectedOutputs[i] = trainingSpheres[i].position.y;
+        }
+        
+        // TrainLinearModelRosenblatt(model, trainingInputs, 2, trainingSpheres.Length, trainingExpectedOutputs, 1, 0.01, 1000)
+    }
+
+    public void PredictOnTestSpheres()
+    {
+        for (var i = 0; i < testSpheres.Length; i++)
+        {
+            var input = new double[] {testSpheres[i].position.x, testSpheres[i].position.z};
+            //var predictedY = PredictXXXLinearModel(model, input, 2)
+            var predictedY = Random.Range(-5, 5);
+            testSpheres[i].position = new Vector3(
+                testSpheres[i].position.x,
+                predictedY,
+                testSpheres[i].position.z);
+        }
+    }
+
+    public void ReleaseModel()
+    {
+        // FreeLinearModel(model);
+    }
+}
---
+++ Unity_Project/Assets/TestDllsScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestDllsScript : MonoBehaviour
+{
+    [DllImport("2020_5A_AL1_CppDllForUnity")]
+    private static extern int GiveMe42FromC();
+    
+    [DllImport("_2020_5A_AL1_MyRustDllForUnity")]
+    private static extern int GiveMe42FromRust();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"MyCDll : {GiveMe42FromC()}");
+        Debug.Log($"MyRustDll : {GiveMe42FromRust()}");
+    }
+}
