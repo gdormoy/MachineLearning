240ecc6cd396e1fcd1f18527c70899ee3bee5193
Nathan Bessa <nathan.besssa@gmail.com>
2020-02-18 17:08:40 +0100

    fix(train): fix linear training

--- library.cpp
+++ library.cpp
@@ -34,11 +34,7 @@ extern "C"{
     DLLEXPORT double* train_linear_class_model(double* model, double* dataset, double* expected_output){
 
     }
-    // create matrixXd with size + 1 col
-    // transpose
-    // transpose dataset and create vector1 (bias)
-    // add both and transpose
-    // regression training in one shot
+
     DLLEXPORT void train_linear_model(double* model, double* dataset, double* expected_output, const int numberOfParams,  const int datasetSize) {
         int size = datasetSize/numberOfParams;
         MatrixXd X(size, numberOfParams + 1);
@@ -50,6 +46,9 @@ extern "C"{
         }
 
         VectorXd Y(size);
+        for(int i = 0; i < size; i++){
+            Y[i] = expected_output[i];
+        }
         VectorXd v = (((X.transpose() * X).inverse() * X.transpose()) * Y).array();
         for(int i = 0; i < numberOfParams + 1; i++){
             model[i] = v[i];
