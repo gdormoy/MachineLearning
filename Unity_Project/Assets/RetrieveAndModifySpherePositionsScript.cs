e408941bc3903694adab6fa0a5ab232a273d48a5
gdormoy <dormoy.guillaume@gmail.com>
2020-02-18 18:52:01 +0100

    fix(regression): huge fix for linear regression

--- Unity_Project/Assets/RetrieveAndModifySpherePositionsScript.cs
+++ Unity_Project/Assets/RetrieveAndModifySpherePositionsScript.cs
@@ -10,7 +10,7 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
     private static extern IntPtr create_linear_model(int numberOfParams);
 
     [DllImport("machine_learning_lib")]
-    private static extern IntPtr train_linear_model(IntPtr model, double[] dataset, double[] expected_output, int numberOfParams,  int datasetSize);
+    private static extern void train_linear_model(IntPtr model, double[] dataset, double[] expected_output, int numberOfParams,  int datasetSize);
 
     [DllImport("machine_learning_lib")]
     private static extern double predict_linear_model(IntPtr model, double[] param, int numberOfParams);
@@ -55,7 +55,8 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
             trainingExpectedOutputs[i] = trainingSpheres[i].position.y;
         }
         
-        train_linear_model(model, trainingInputs, trainingExpectedOutputs, 2, trainingSpheres.Length);
+        train_linear_model(model, trainingInputs, trainingExpectedOutputs, 2, trainingInputs.Length);
+        Debug.Log("model is trainong");
     }
 
     public void PredictOnTestSpheres()
@@ -63,7 +64,11 @@ public class RetrieveAndModifySpherePositionsScript : MonoBehaviour
         for (var i = 0; i < testSpheres.Length; i++)
         {
             var input = new double[] {testSpheres[i].position.x, testSpheres[i].position.z};
+            Debug.Log($"x: {input[0]}");
+            Debug.Log($"z: {input[1]}");
+            Debug.Log($"input length: {input.Length}");
             var predictedY = (float) predict_linear_model(model, input, 2);
+            Debug.Log($"predict: {predictedY}");
             // var predictedY = Random.Range(-5, 5);
             testSpheres[i].position = new Vector3(
                 testSpheres[i].position.x,
--- library.cpp
+++ library.cpp
@@ -41,17 +41,23 @@ extern "C"{
         for(int i = 0; i < size; i++){
             X(i, 0) = 1;
             for(int j = 1; j < numberOfParams + 1; j++){
-                X(j,i) = dataset[i * numberOfParams + j];
+                X(i,j) = dataset[i * numberOfParams + j - 1];
             }
         }
 
-        VectorXd Y(size);
+        std::cout << "X : " << X << endl;
+
+        MatrixXd Y(size, 1);
         for(int i = 0; i < size; i++){
-            Y[i] = expected_output[i];
+            Y(i, 0) = expected_output[i];
         }
-        VectorXd v = (((X.transpose() * X).inverse() * X.transpose()) * Y).array();
+
+        std::cout << "Y : " << Y << endl;
+        auto v = (((X.transpose() * X).inverse() * X.transpose()) * Y);
+
+        std::cout << "W : " << v << endl;
         for(int i = 0; i < numberOfParams + 1; i++){
-            model[i] = v[i];
+            model[i] = v(i, 0);
         }
     }
 
@@ -67,4 +73,20 @@ extern "C"{
     DLLEXPORT double predict_linear_class_model(const double* model, const double* params, const int numberOfParams) {
         return signbit(predict_linear_model(model, params, numberOfParams));
     }
+}
+
+int main()
+{
+    auto dataset = new double[6] {-10.0, 0.0, 1.0, 0.0, 5.0, 1.0};
+    auto expectedOutputs = new double[3] {-30.0, 42.0, 1.0};
+
+    auto m = create_linear_model(2);
+    train_linear_model(m, dataset, expectedOutputs, 2, 6);
+
+    cout << predict_linear_model(m, dataset, 2) << endl;
+    cout << predict_linear_model(m, dataset + 2, 2) << endl;
+    cout << predict_linear_model(m, dataset + 4, 2) << endl;
+
+    std::cout << "feoizjfozeijfoizej" << endl;
+    return 0;
 }
