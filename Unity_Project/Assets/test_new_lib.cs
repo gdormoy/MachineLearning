4fc6ec6a50fbb31cb55c53ac376bab2f250c8738
Thomas Gianfalla <gianfalla.t@gmail.com>
2020-02-18 14:20:11 +0100

    creat .dll and test Script

---
+++ Unity_Project/Assets/test_new_lib.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class test_new_lib : MonoBehaviour
+{
+    [DllImport("machine_learning_lib")]
+    private static extern double create_linear_model();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"MyCDll2 : {create_linear_model()}");
+    }
+}
