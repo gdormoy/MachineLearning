169ceeede5554792774eabf5a97119b0394e6221
Thomas Gianfalla <gianfalla.t@gmail.com>
2020-02-18 11:39:42 +0100

    Add all test case

--- Unity_Project/Assets/TestDllsScript.cs
+++ Unity_Project/Assets/TestDllsScript.cs
@@ -6,13 +6,9 @@ public class TestDllsScript : MonoBehaviour
     [DllImport("2020_5A_AL1_CppDllForUnity")]
     private static extern int GiveMe42FromC();
     
-    [DllImport("_2020_5A_AL1_MyRustDllForUnity")]
-    private static extern int GiveMe42FromRust();
-    
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"MyCDll : {GiveMe42FromC()}");
-        Debug.Log($"MyRustDll : {GiveMe42FromRust()}");
     }
 }
